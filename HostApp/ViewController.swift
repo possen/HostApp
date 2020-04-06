da170cfef894ea6df2d3235ea78abfebc14e4ead
Paul Ossenbruggen <possen@gmail.com>
2020-04-05 23:40:19 -0700

    Initial Commit

---
+++ HostApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HostApp
+//
+//  Created by Paul Ossenbruggen on 4/5/20.
+//  Copyright © 2020 Paul Ossenbruggen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
